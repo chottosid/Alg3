30a61547e87709a58e6ff2e102b9ae83d7388a2f
swarup <swarupsidhartho@gmail.com>
2026-04-05 22:32:18 +0600

    init and working

---
+++ build/CMakeFiles/agp_solver.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/agp_solver.dir/src/main.cpp.o"
+  "CMakeFiles/agp_solver.dir/src/main.cpp.o.d"
+  "agp_solver"
+  "agp_solver.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/agp_solver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
